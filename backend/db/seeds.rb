deb087cb7f8d26190f49bbe7c5369fa71b0763ac
Patrick Baselier <patrick@kabisa.nl>
2014-07-24 18:37:07 +0200

    Add Product resource to backend

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy_all
+
+100.times do
+  Product.create do |product|
+    product.title = Faker::Lorem.word
+  end
+end
