c739727a73355033cb0f31531d2bb6926e8a7cd5
Patrick Baselier <patrick@kabisa.nl>
2014-07-29 08:33:05 +0200

    Update to ember-cli 0.0.40

--- frontend/tests/helpers/start-app.js
+++ frontend/tests/helpers/start-app.js
@@ -2,6 +2,7 @@
 
 var Application = require('shop/app')['default'];
 var Router = require('shop/router')['default'];
+import Ember from 'ember';
 
 export default function startApp(attrs) {
   var App;
