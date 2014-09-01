d5d7dbb9181685824da2b4ba7afb56bdad8d4b7d
Jeroen Knoops <jeroen.knoops@kabisa.nl>
2014-09-01 21:45:43 +0200

    Rollback model when dirty..

---
+++ frontend/app/routes/product.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+     willTransition: function(transition) {
+       var product = this.modelFor(this.routeName);
+       if(product.get('isDirty')) {
+         product.rollback();
+       }
+    }
+  }
+});
