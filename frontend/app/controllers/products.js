538965b3ca29a29679fba53243ba74a37c0bfc2b
Patrick Baselier <patrick@kabisa.nl>
2014-09-26 15:19:38 +0200

    Reset query params from navbar

--- frontend/app/controllers/products.js
+++ frontend/app/controllers/products.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   queryParams: ['category'],
-  category: null,
+  category: '', // using an empty string as the default property value removes the query params from the link-to-generated URL
+                // https://github.com/emberjs/ember.js/issues/5274#issuecomment-50454222
   categories: Ember.computed.mapBy('model', 'category'),
   uniqCategories: Ember.computed.uniq('categories'),
   filteredContent: function() {
